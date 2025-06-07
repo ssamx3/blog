be515fe1b29bb376c5ab140e4ad6f70f80514fb0
sam!! <72251349+ssamx3@users.noreply.github.com>
2025-06-07 11:29:04 +0100

    aaa

---
+++ src/posts/post1.md
@@ -0,0 +1,13 @@
+---
+title: nettles
+description: ethel cain
+date: '6-6-2025'
+categories:
+  - music
+published: true
+---
+# we were in a race
+to grow up
+yesterday to today until tomorrow \
+_but_ when the plant blew up \
+a piece of shrapnel came and slowed that part of you 
--- svelte.config.js
+++ svelte.config.js
@@ -2,6 +2,13 @@ import adapter from '@sveltejs/adapter-auto';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
+import { mdsvex } from 'mdsvex'
+
+/** @type {import('mdsvex').MdsvexOptions} */
+const mdsvexOptions = {
+	extensions: ['.md'],
+}
+
 const config = {
 	// Consult https://svelte.dev/docs/kit/integrations
 	// for more information about preprocessors
