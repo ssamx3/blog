8ad9b9715bfeb1f2cedfa807ffad5454826d2263
sam!! <72251349+ssamx3@users.noreply.github.com>
2025-06-09 21:38:51 +0100

    prettier formatted

--- src/routes/api/posts/server.ts
+++ src/routes/api/posts/server.ts
@@ -1,13 +1,15 @@
-import { json} from "@sveltejs/kit";
-import type { Post } from '$lib/Post'
+import {json} from "@sveltejs/kit";
+import type {Post} from '$lib/Post'
 
 async function getPosts() {
     let posts: Post[] = []
 
     const paths = import.meta.glob('src/posts/*.md', {eager: true})
 
-    for(const path in paths) {
+    for (const path in paths) {
         const file = paths[path]
-        const slug =  path.split('/').at(-1)?.replace('.md', '')
+        const slug = path.split('/').at(-1)?.replace('.md', '')
+
+        if (file)
     }
-    }
+}
